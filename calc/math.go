b2f3a1d7ff608d48459a96ffb9be29d8a2b1ea81
Alfredo Magalhaes <alfredo.magalhaes@outlook.com>
2020-09-26 12:39:12 -0300

    Função Add alterada para receber um número variado de parâmetros

--- calc/math.go
+++ calc/math.go
@@ -1,6 +1,12 @@
 package calc
 
-//Add - Função para somar dois valores
-func Add(i, j int) int {
-	return i + j
+//Add - Função para somar todos os valores passados por parâmetro
+func Add(numbers ...int) int {
+	sum := 0
+
+	for _, num := range numbers {
+		sum = sum + num
+	}
+
+	return sum
 }
