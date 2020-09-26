8706ef7be0685e8c90dc1b1b759b841c86101c50
Alfredo Magalhaes <alfredo.magalhaes@outlook.com>
2020-09-26 19:06:20 -0300

    Método de somatória alterado para retornar erro caso os parametros estejam incorretos

--- calc/math.go
+++ calc/math.go
@@ -1,12 +1,18 @@
 package calc
 
-//Add - Função para somar todos os valores passados por parâmetro
-func Add(numbers ...int) int {
+import "errors"
+
+//Add - Função para somar todos os valores passados por parâmetro e um código de erro
+func Add(numbers ...int) (int, error) {
 	sum := 0
 
-	for _, num := range numbers {
-		sum = sum + num
+	if len(numbers) < 2 {
+		return sum, errors.New("Numero insuficiente de parametros, informar no minimo 2 números")
+	} else {
+		for _, num := range numbers {
+			sum = sum + num
+		}
 	}
 
-	return sum
+	return sum, nil
 }
